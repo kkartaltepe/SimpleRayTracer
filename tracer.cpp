839ac10c203ad04589ddfa09edea8333fe0baf37
kkartaltepe <kkartaltepe@gmail.com>
2014-11-30 18:20:47 -0600

    Simple scene showing lights, triangles, circles

--- tracer.cpp
+++ tracer.cpp
@@ -13,26 +13,22 @@ SceneGraph scene;
 glm::vec3 trace(Ray ray);
 
 void initSceneData() {
-  // triangles.push_back(Triangle(glm::vec3(50.0f, -100.0f, 100.0f),
-  //                              glm::vec3(100.0f, 100.0f, 100.0f),
-  //                             //  glm::vec3(-100.0f, 100.0f, 100.0f)));
-  scene.addTriangle(Triangle(glm::vec3(0.0f, 0.5f, -2.0f),
-                               glm::vec3(-0.5f, -0.5f, -2.0f),
-                               glm::vec3(0.5f, -0.5f, -2.0f)));
-  //scene.load("cube.obj");
-  // scene.addCircle(Circle(glm::vec3(0.0f, 0.0f, 0.0f), 1.0f));
-  scene.addCircle(Circle(glm::vec3(0.0f, 0.0f, -2.0f), 0.2f));
-  scene.addLight(Light(glm::vec3(0.0f, 0.1f, -5.0f),
-                          glm::vec3(0.0f, 0.0f, 40.0f)));
-  // scene.addLight(Light(glm::vec3(0.0f, 200.0f, -200.0f),
-  //                         glm::vec3(0.0f, 8000.0f, 0.0f)));
-  // scene.addLight(Light(glm::vec3(200.0f, 200.0f, 0.0f),
-  //                         glm::vec3(8000.0f, 0.0f, 0.0f)));
+  scene.load("cube.obj");
+  scene.addCircle(Circle(glm::vec3(1.5f, 1.5f, -1.5f), 0.2f));
+  scene.addCircle(Circle(glm::vec3(0.0f, 1.5f, -1.5f), 0.2f));
+  scene.addCircle(Circle(glm::vec3(1.5f, 0.0f, -1.5f), 0.2f));
+  scene.addCircle(Circle(glm::vec3(1.5f, 1.5f, 0.0f), 0.2f));
+  scene.addLight(Light(glm::vec3(2.0f, 0.0f, -2.0f),
+                          glm::vec3(0.0f, 0.0f, 8.0f)));
+  scene.addLight(Light(glm::vec3(0.0f, 2.0f, -2.0f),
+                          glm::vec3(0.0f, 8.0f, 0.0f)));
+  scene.addLight(Light(glm::vec3(2.0f, 2.0f, 0.0f),
+                          glm::vec3(8.0f, 0.0f, 0.0f)));
   scene.setCamera(Camera(
-    glm::vec3(4.0, 0.0f, -7.0f),
+    glm::vec3(3.0, 3.0f, -3.0f),
     glm::vec3(0.0f, 0.0f, 0.0f),
     glm::vec3(0.0f, 1.0f, 0.0f),
-    30.0f, PROJ_WIDTH, PROJ_HEIGHT));
+    60.0f, PROJ_WIDTH, PROJ_HEIGHT));
 
 }
 
